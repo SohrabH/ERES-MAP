a832fa5fc8b15dd47c6dacb4a786c9c4c82c1c57
Sohrab Hussain <sohrab.hussain24@yahoo.in>
2014-11-22 21:05:00 +0400

    Initial Commit

---
+++ ERES-MAP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ERES-MAP
+//
+//  Created by Sohrab Hussain on 22/11/14.
+//  Copyright (c) 2014 DLD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ERES-MAP/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ERES-MAP
+//
+//  Created by Sohrab Hussain on 22/11/14.
+//  Copyright (c) 2014 DLD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
